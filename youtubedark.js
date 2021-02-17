9375d102abb16b1fc916f809c049a8356258776d
Etstansfield <etstansfield@hotmail.com>
2021-02-17 15:34:16 +0000

    Check for if removed on page load
    Check if after page load it has removed it - if so reapply

--- youtubedark.js
+++ youtubedark.js
@@ -1,2 +1,16 @@
 var htmlTag = document.getElementsByTagName("html")[0];
 htmlTag.setAttribute("dark", "true");
+
+// attaching a listener to page load didn't seem to work for some reason - so doing it the lazy way
+var execute = function () {
+ htmlTag.setAttribute("dark", "true");
+ 
+ if (document.readyState !== 'complete') {
+	setTimeout(execute, 3000);	// recursivly call until complete
+ }
+ 
+};
+
+// Incase it removes it for some reason - it seems to do this on large vids
+setTimeout(execute, 3000);
+
