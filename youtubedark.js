c8065d91864631d1a1123e04e8a483d2c5e735fb
Etstansfield <etstansfield@hotmail.com>
2021-03-03 19:05:34 +0000

    1.2 update
    lowered timer

--- youtubedark.js
+++ youtubedark.js
@@ -4,13 +4,9 @@ htmlTag.setAttribute("dark", "true");
 // attaching a listener to page load didn't seem to work for some reason - so doing it the lazy way
 var execute = function () {
  htmlTag.setAttribute("dark", "true");
- 
- if (document.readyState !== 'complete') {
-	setTimeout(execute, 3000);	// recursivly call until complete
- }
- 
+ setTimeout(execute, 1000);
 };
 
 // Incase it removes it for some reason - it seems to do this on large vids
-setTimeout(execute, 3000);
+setTimeout(execute, 1000);
 
